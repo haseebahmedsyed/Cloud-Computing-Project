caa4c2be73af69ab4af73b770e0606d64e960192
Murtaza Khan <murtaza10dec@gmail.com>
2024-06-01 10:39:25 +0500

    Security compliance added

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router()
-import { authUser, deleteUser, getUserById, getUserProfile, getUsers, registerUser, updateUser, updateUserProfile } from '../controllers/userController.js'
+import { authUser, deleteUser, getUserById, getUserPassword, getUserProfile, getUserProfileByEmail, getUsers, registerUser, updateUser, updateUserProfile } from '../controllers/userController.js'
 import {admin, protect} from "../middleware/authMiddleware.js"
 
 
 router.route('/').post(registerUser).get(protect,admin, getUsers)
+router.route('/password').get(protect, getUserPassword)
+router.route('/password/:email').get(getUserProfileByEmail)
 router.route('/login').post(authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 router.route('/:id').delete(protect,admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,8 @@
 import asyncHandler from 'express-async-handler'
 import User from "../models/userModel.js";
 import generateToken from '../utils/generateToken.js';
-
+import nodemailer from 'nodemailer';
+// import { transporter } from '../utils/mail.js';
 
 const authUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body
@@ -20,39 +21,56 @@ const authUser = asyncHandler(async (req, res) => {
         throw new Error("Invalid email or Password")
     }
 })
-
-
 const registerUser = asyncHandler(async (req, res) => {
-    const {name, email, password} = req.body
-    const userExists = await User.findOne({email})
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
 
-    if(userExists){
-        res.status(400)
-        throw new Error("User already exists")
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
     }
 
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAIL,
+            pass: process.env.PASSWORD,
+        },
+    });
+    const user = await User.create({ name, email, password });
 
-    const user = await User.create({name, email, password})
+    if (user) {
+        // Send registration email
+        const mailOptions = {
+            from: process.env.MAIL,
+            to: user.email,
+            subject: 'Welcome to ProShop',
+            text: `Hello ${name},\n\nWelcome to ProShop! We have registered your information securely on our website. Enjoy shopping from ProShop and have a wonderful experience.\n\nBest Regards,\nThe ProShop Team`
+        };
 
-    if (user){
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(`Error sending email: ${error}`);
+            } else {
+                console.log(`Email sent: ${info.response}`);
+            }
+        });
+
+        console.log("Mail ", process.env.MAIL)
+        console.log("pass ", process.env.PASSWORD)
+
         res.status(201).json({
-            _id : user._id,
+            _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: generateToken(user._id)
-        })
-    }else{
-        res.status(400)
-        throw new Error("Invalid user data")
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
     }
-
-    
-
-    
-
-    
-})
+});
 
 
 const getUserProfile = asyncHandler(async (req, res) => {
@@ -64,6 +82,46 @@ const getUserProfile = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin
+        })
+
+    }
+    else{
+        res.status(404)
+        throw new Error("User not found")
+        
+    }
+})
+
+const getUserProfileByEmail = asyncHandler(async (req, res) => {
+    const email = req.params.email;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+});
+
+
+
+
+const getUserPassword = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    
+    if (user){
+        res.json({
+            _id : user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password
         })
 
     }
@@ -162,4 +220,4 @@ const updateUser = asyncHandler(async (req, res) => {
 })
 
 
-export {authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser}
+export {authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser, getUserPassword, getUserProfileByEmail}
